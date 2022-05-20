e71c996fb9b7ff23111cab40a2ae0f9a0bd83dbd
azurba <joaoh.pimenta.giudice@gmail.com>
2022-05-20 09:59:03 -0600

    .

--- 01_tutorial/src/App.js
+++ 01_tutorial/src/App.js
@@ -2,21 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //variable definition
+  const name = "Joao";
+  //function definition
+  const handleNameChange = () => {
+    const names = ['Joao','Henrique', 'Pimenta', 'Giudice']
+    const int = Math.floor(Math.random() * 4)
+    return names[int];
+  }
+  //loading variable and functions to 'html' code:
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {name} 
+          {handleNameChange()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
