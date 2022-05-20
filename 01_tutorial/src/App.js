43cb3745869f5eabe0f77b8fa24deacad2549a44
azurba <joaoh.pimenta.giudice@gmail.com>
2022-05-20 10:15:29 -0600

    add footer

--- 01_tutorial/src/App.js
+++ 01_tutorial/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   //variable definition
   const name = "Joao";
   
-  
   //loading variable and functions to 'html' code:
+  //Custom tags are defined in different files.
   return (
     <div className="App">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
